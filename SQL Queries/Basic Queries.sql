07cb83a78895441b4c6219b58b254bafc3ed6bac
Lashawn Fofung <lashawn.fofung@gmail.com>
2025-07-02 20:20:34 -0400

    Update Basic Queries.sql

--- SQL Queries/Basic Queries.sql
+++ SQL Queries/Basic Queries.sql
@@ -4,8 +4,8 @@ Super Heroes Dataset Exploration
 Skills used: SELECT, GROUP, ORDER
 */
 
-/*Publisher v Number of Heroes*/
-/*To find out how many many heroes belong to each publisher*/
+/*Publisher v Number of Heroes
+- To find out how many many heroes belong to each publisher*/
 SELECT Publisher, COUNT(*) AS NumberOfHeroes
 FROM heroes_information
 GROUP BY Publisher
